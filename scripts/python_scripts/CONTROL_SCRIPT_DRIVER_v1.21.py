6587f5d640bd0421da91c7ac5c95f9604439a5af
Isaac Amboson <isaacamboson@gmail.com>
2024-02-17 07:28:53 -0500

    Adding comments

--- scripts/python_scripts/CONTROL_SCRIPT_DRIVER_v1.21.py
+++ scripts/python_scripts/CONTROL_SCRIPT_DRIVER_v1.21.py
@@ -483,7 +483,7 @@ if __name__ == "__main__":
 				sm.add_user_to_group(service = sys.argv[2], group_name = sys.argv[3], user_name = db_user)
 
 
-			#loggind endtime of operation into the database
+			#logging endtime of operation into the database
 			OP_STATUS = "COMPLETED"
 			if OP_STATUS == "COMPLETED":
 				timestring3 = datetime.datetime.now()
@@ -515,7 +515,7 @@ if __name__ == "__main__":
 		print("Please run this script in the format below:")
 		print("python *scriptname* *operation* *service* *group name* *runner* *OP_NAME* *OP_STATUS*")
 
-		#rainy day scenario for when operation is wrongly selected. Please select right operation and provide correct command line arguments
+	#rainy day scenario for when operation is wrongly selected. Please select right operation and provide correct command line arguments
 	else:
 		print("Please select operation to perform: 'backup', 'database_backup', 'disk_utilization', 'database_import', 'database_migration', 'create_aws_user', 'create_aws_group' or 'add_user_to_group'")
 
